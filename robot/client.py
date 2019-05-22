3375f05ba06a7df0ad20e42f3345b6db45581d92
evgeniy.scherbina <evgeniy.scherbina@bitfury.com>
2019-05-22 18:21:25 +0300

    added simple example

---
+++ robot/client.py
@@ -0,0 +1,15 @@
+import robot_pb2
+import robot_pb2_grpc
+
+import grpc
+
+def run():
+    # NOTE(gRPC Python Team): .close() is possible on a channel and should be
+    # used in circumstances in which the with statement does not fit the needs
+    # of the code.
+    channel = grpc.insecure_channel('localhost:8080')
+    stub = robot_pb2_grpc.RobotServiceStub(channel)
+    response = stub.PutEnvironmentInfo(robot_pb2.EnvironmentInfo(EnvironmentTemp=1))
+    # print("Robot client received: " + response)
+
+run()
